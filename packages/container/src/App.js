79ae5de0fc02c50ba633eb3733296aec92ebd4f8
Li Zhou <lizhou@MacBook-Pro.local>
2020-12-27 19:33:37 +1100

    create container yml4

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
     return <div>
-        <h1>Hi container</h1>
+        <h1>Hi Contanter</h1>
         <hr/>
         <MarketingApp />
     </div>
