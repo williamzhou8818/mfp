f6c1cd6e396bcfcacfd9df138debc39c3aa6f1fd
Li Zhou <lizhou@MacBook-Pro.local>
2020-12-27 16:28:18 +1100

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+// import { mount as MarketingMount } from 'marketing/MarketingApp';
+// console.log(MarketingMount)
+
+export default () => {
+    return <div>
+        <h1>Hi container</h1>
+        <hr/>
+        <MarketingApp />
+    </div>
+};
