3e14cba92dac2ac8a425039fac3f1e93bdd4f3e1
Li Zhou <lizhou@MacBook-Pro.local>
2020-12-28 10:48:59 +1100

    change container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi configure-aws-credentials@v1 aws!</h1>
+      <h1>Hi ap-southeast-2aws!</h1>
       <hr />
       <MarketingApp />
     </div>
