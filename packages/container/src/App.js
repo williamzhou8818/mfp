a21f814a9a5efa29092f248879baafd4fa96117d
Li Zhou <lizhou@MacBook-Pro.local>
2020-12-28 13:34:20 +1100

    add destrube again

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>H-southeast2-chagne-destrueb</h1>
+      <h1>H-southeast2-chagne-dest-agian</h1>
       <hr />
       <MarketingApp />
     </div>
