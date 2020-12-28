0495ace0c9e23bac67bce21224eaae953ef89f81
Li Zhou <lizhou@MacBook-Pro.local>
2020-12-28 15:44:01 +1100

    add ivnali not work

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi-changetsIDfsfstgine</h1>
+      <h1>Hi-</h1>
       <hr />
       <MarketingApp />
     </div>
