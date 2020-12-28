6d50d6876280188e7479b5759319f0737cad2b28
Li Zhou <lizhou@MacBook-Pro.local>
2020-12-28 15:13:57 +1100

    add ivnalideta

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>H-soheasttest2</h1>
+      <h1>H-soheatest2</h1>
       <hr />
       <MarketingApp />
     </div>
