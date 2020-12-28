e949391245269ac0d36c160fab9dc27511d4bbc4
Li Zhou <lizhou@MacBook-Pro.local>
2020-12-28 11:03:12 +1100

    change container- run: aws s3

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi runaws s3 ls</h1>
+      <h1>Hi aws-cli@v1.18.43</h1>
       <hr />
       <MarketingApp />
     </div>
