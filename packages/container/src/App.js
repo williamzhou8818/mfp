e2eb96381ebc3712728c0fd7891a96c250300b9b
Li Zhou <lizhou@MacBook-Pro.local>
2020-12-28 11:23:08 +1100

    change container- ap-southeast33

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi ap-southeast2-2</h1>
+      <h1>Hi -southeast2-2</h1>
       <hr />
       <MarketingApp />
     </div>
