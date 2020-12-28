82fc23eadf974492788a6a5d6cb5e143c6ce01e7
Li Zhou <lizhou@MacBook-Pro.local>
2020-12-28 16:12:31 +1100

    add ivnal19

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi-try-again2-idn</h1>
+      <h1>Hi-try-again2-id</h1>
       <hr />
       <MarketingApp />
     </div>
