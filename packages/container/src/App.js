e3f825f648819388f283aabb66997d277d5779cc
Li Zhou <lizhou@MacBook-Pro.local>
2020-12-28 14:38:28 +1100

    add destr

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>H-southeast-changed-changed</h1>
+      <h1>H-southeast-test</h1>
       <hr />
       <MarketingApp />
     </div>
