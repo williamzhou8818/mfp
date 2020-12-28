d2ad71a5924812a9e19f66dcdd46d76225ee89a3
Li Zhou <lizhou@MacBook-Pro.local>
2020-12-28 14:58:36 +1100

    add des

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>H-soheast-test2</h1>
+      <h1>H-soheasttest2</h1>
       <hr />
       <MarketingApp />
     </div>
